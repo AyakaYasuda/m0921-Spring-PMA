3bd4ec700bea5cf8ffe8b8a403fcc87d5c41223d
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-11-17 02:05:21 -0800

    edited home page and added chart

--- src/main/java/com/spring/pma/controller/HomeController.java
+++ src/main/java/com/spring/pma/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.spring.pma.controller;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.pma.dao.iEmployeeRepository;
 import com.spring.pma.dao.iProjectRepository;
+import com.spring.pma.dto.ChartData;
+import com.spring.pma.dto.EmployeeProject;
 
 
 @Controller
@@ -20,7 +25,17 @@ public class HomeController {
 	iEmployeeRepository empRepo;
 	
 	@GetMapping("/")
-	public String displayHome(Model model) {
+	public String displayHome(Model model) throws JsonProcessingException {
+		List<EmployeeProject> employeeProjCount = empRepo.employeeProject();
+		model.addAttribute("employeeList", employeeProjCount);
+		
+		List<ChartData> projectData = proRepo.getProjectStatus();
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString =  objectMapper.writeValueAsString(projectData);
+		// [["label":"NOTSTARTED", "value":1] , ["label":"INPROGRESS, "value":1], ["label":"COMPLETED", "value":1]]
+		
+		model.addAttribute("projectStatusCount", jsonString);
+		
 		return "main/home";
 	}
 
--- src/main/java/com/spring/pma/entity/Project.java
+++ src/main/java/com/spring/pma/entity/Project.java
@@ -16,7 +16,7 @@ import javax.persistence.ManyToMany;
 public class Project {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long projectId;
 	private String name;
 	private String stage; //INPROGRESS, NOTSTARTED, COMPLETED
